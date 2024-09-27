160b3d7319c00fa00078873af8b9eb817eaf3405
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-27 16:36:09 +0300

    Print statement was used.

--- Section22.Try-catch/throwError.sql
+++ Section22.Try-catch/throwError.sql
@@ -4,9 +4,11 @@ GO
 
 CREATE PROC NewAverageBalance(@EmployeeNumberFrom INT, @EmployeeNumberTo INT, @AverageBalance INT OUTPUT) AS
 BEGIN
-	DECLARE @TotalAmount MONEY
+	SET NOCOUNT ON
+	DECLARE @TotalAmount DECIMAL(5,2)
 	DECLARE @NumberOfEmployee INT
 	BEGIN TRY 
+      PRINT 'The employees numbers are from ' + CONVERT(NVARCHAR(10), @EmployeeNumberFrom) + ' to ' + CONVERT(NVARCHAR(10), @EmployeeNumberTo)
 	  SELECT @TotalAmount = SUM(Amount) FROM [tblTransaction]
 	  WHERE [EmployeeNumber] BETWEEN @EmployeeNumberFrom AND @EmployeeNumberTo
 	  SELECT @NumberOfEmployee = COUNT(DISTINCT [EmployeeNumber]) FROM [tblEmployee]
@@ -16,10 +18,21 @@ BEGIN
 	END TRY
 	BEGIN CATCH
 		SET @AverageBalance = 0
-		SELECT ERROR_MESSAGE() AS [ErrorLine],
-			   ERROR_NUMBER() AS [ErrorNumber], ERROR_PROCEDURE() AS [ErrorProcedure],
-			   ERROR_SEVERITY() AS [ErrorSeverity], ERROR_STATE() AS [ErrorState]
-		RETURN 1
+		IF ERROR_NUMBER() = 8134
+			BEGIN
+				SET @AverageBalance = 0
+				RETURN 8134
+			END
+		ELSE 
+			DECLARE @Error_Message AS NVARCHAR(255)
+			SELECT @Error_Message = ERROR_MESSAGE()
+			RAISERROR(@ErrorMessage, 16, 1)
+		--SELECT ERROR_MESSAGE() AS [ErrorLine],
+		--	   ERROR_NUMBER() AS [ErrorNumber], ERROR_PROCEDURE() AS [ErrorProcedure],
+		--	   ERROR_SEVERITY() AS [ErrorSeverity], ERROR_STATE() AS [ErrorState],
+
+		--	   ERROR_STATE() AS [ErrorState]
+		--RETURN 1
 	END CATCH
 END
 GO
@@ -31,5 +44,10 @@ GO
 
 DECLARE @AvgBalance INT, @ReturnStatus INT
 EXEC @ReturnStatus = NewAverageBalance 223, 227, @AvgBalance OUTPUT
+SELECT @AvgBalance AS [Average_Balance], @ReturnStatus AS [Return_Status], 'Error did not stop us' AS [MyMessage]
+GO
+
+DECLARE @AvgBalance INT, @ReturnStatus INT
+EXEC @ReturnStatus = NewAverageBalance 323, 327, @AvgBalance OUTPUT
 SELECT @AvgBalance AS [Average_Balance], @ReturnStatus AS [Return_Status]
 GO
