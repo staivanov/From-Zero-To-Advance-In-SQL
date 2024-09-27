c4cf4a61285e82f7100c7fc700d968a2494bb94d
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-27 16:39:14 +0300

    Section 17. Triggers was filled with demos.

---
+++ Section17.CreateAndAlterTriggers/createTrigger.sql
@@ -0,0 +1,11 @@
+CREATE TRIGGER tr_tblDepartment
+	ON [dbo].[tblDepartment]
+	AFTER DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+	SET NOCOUNT ON
+	END
+
+SET NOCOUNT ON
+SELECT * FROM [tblDepartment]
+SET NOCOUNT OFF
