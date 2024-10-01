1dfc143dffce500336d22aaa17914d164231b77d
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-10-01 08:55:01 +0300

    Correction to sql query.

--- Section25.ImplementAggregateQueries/rowsBetween.sql
+++ Section25.ImplementAggregateQueries/rowsBetween.sql
@@ -1,7 +1,7 @@
 SELECT A.[EmployeeNumber], A.[AttendanceMonth], A.[NumberAttendance], SUM(A.[NumberAttendance])
 OVER (PARTITION BY A.[EmployeeNumber], YEAR (A.[AttendanceMonth])
 	ORDER BY A.[AttendanceMonth], A.[NumberAttendance]
-	ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) AS [RunningTotal]
+	ROWS BETWEEN 0 PRECEDING AND 1 FOLLOWING) AS [RunningTotal]
 FROM [tblEmployee] AS E
-JOIN [tblAttendance] AS A
+JOIN  [70-461].[dbo].[tblAttendance] AS A
 ON E.[EmployeeNumber] = A.[EmployeeNumber]
